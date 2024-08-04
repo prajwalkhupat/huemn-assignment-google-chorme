1ef43f77ef2b11e6ba6736ed076692c5a1852c28
prajwal khupat <prajwalkhupat5@gmail.com>
2024-08-04 14:25:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import HomeSection from "./sections/HomeSection";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <HomeSection />
+    </div>
+  );
+};
+
+export default App;
